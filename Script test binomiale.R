461804ab4e72c1d941c742878cb6cd53bc8efc50
Tsarai <eliot.halouani@gmail.com>
2026-06-05 12:00:38 +0200

    ajout du script pour les tests binomiales et avancement sur les script pour le test de fischer

--- Script test binomiale.R
+++ Script test binomiale.R
@@ -28,8 +28,8 @@ for(a in seq(0, 1, by = 0.05)){
   vect_a = c(vect_a,conditionnal_test_under_null(a,100,10000))
 }
 plot(x,vect_a)
+abline(0,1,col='red')
 
-?rpois
 unconditionnal_test = function(a,p,b){
   Y1 = rpois(b,p[1])
   Y2 = rpois(b,p[2])
@@ -45,7 +45,6 @@ unconditionnal_test = function(a,p,b){
     vect_Y1[i]=0
     }
   }
-'print(vect_n00)' 
 return(sum(vect_Y1)/b)
 }
 
@@ -58,3 +57,92 @@ for(a in seq(0, 1, by = 0.05)){
   vect_a2 = c(vect_a2,unconditionnal_test(a,c(8,10),10000))
 }
 plot(x,vect_a2)
+
+#fonction qui calcul distribution de la p-values, génére B p-values indépendante 
+conditionnal_pvalue_under_null = function(n,b,l){
+  vect_pval=c()
+  vect_Bpval=c()
+  vect_repF=c()
+  vect_repE=c()
+  tab = rbinom(b,n,1/2)
+  support = 0:n
+  for(k in support){
+    pi = 0
+    for(i in k:n){
+      pi = pi + choose(n,i)*1/2**n
+    }
+    vect_pval = c(vect_pval,pi)
+  }
+  for(i in tab){
+    vect_Bpval=c(vect_Bpval,vect_pval[match(i,support)])
+  }
+  for(t in rev(vect_pval)){
+    pi <- 0
+    for(k in support){
+      if(vect_pval[k+1] <= t){
+        pi <- pi +
+          choose(n,k)*(l/(l+l))^k*(l/(l+l))^(n-k)
+      }
+    }
+    vect_repF <- c(vect_repF, pi)
+  }
+  for(i in rev(vect_Bpval)){
+    pi=0
+    pi <- mean(vect_Bpval <= i)
+    vect_repE = c(vect_repE,pi)
+  }
+  par(mfrow = c(1,2))
+  
+  plot(support,vect_pval,xlab = "k",ylab = "p(k)",main = "P-values théoriques")
+  #lines(vect_Bpval,type='p',col='blue')
+
+  t_vals = sort(unique(vect_pval))
+  vect_repF_unique = sapply(t_vals,function(t) sum(dbinom(support, n, 1/2)[vect_pval <= t]))
+  F_theo = stepfun(t_vals,c(0, vect_repF_unique))
+  F_emp = ecdf(vect_Bpval)
+  plot(F_theo,do.points = FALSE,verticals = TRUE,xlim = c(0,1),ylim = c(0,1),xlab = "t",ylab = "F(t)")
+  abline(0, 1, col = "red", lwd = 2)
+  lines(F_emp,verticals = TRUE,do.points = FALSE,lty = 2,type ='l')
+}
+conditionnal_pvalue_under_null(1000,100,3)
+
+unconditionnal_pvalue = function(a,l1,l2,b){
+  Y1 = rpois(b,l1)
+  Y2 = rpois(b,l2)
+  Y1_Y2 = Y1+Y2
+  vect_pval = c()
+  for(i in 1:b){
+    pv=0
+    for(j in Y1[i]:Y1_Y2[i]){
+      pv = pv+choose(Y1_Y2[i],j)*1/2**Y1_Y2[i]
+    }
+    vect_pval = c(vect_pval,pv)
+  }
+  for (i in 1:b){
+    if (vect_pval[i]<=a){
+      vect_pval[i]=1
+    }
+    else{
+      vect_pval[i]=0
+    }
+  }
+  risk_exp=sum(vect_pval)/b
+  return(risk_exp)
+}
+unconditionnal_pvalue(0.05,3,2,1000)
+
+#visualisation du risque de rejet epirique en fonction de la valeur de l1
+vect_risk = c()
+x=seq(0, 40, by = 1)
+for(i in x){
+  vect_risk=c(vect_risk,unconditionnal_pvalue(0.05,i,20,1000))
+}
+plot(x,vect_risk)
+
+#visualisation du risque de rejet epirique en fonction du niveau du test
+vect_a=c()
+x=seq(0, 1, by = 0.05)
+for(a in x){
+  vect_a = c(vect_a,unconditionnal_pvalue(a,2,3,1000))
+}
+plot(x,vect_a)
--- scipr stage L2.R
+++ scipr stage L2.R
@@ -1,4 +1,4 @@
-
+#Fonction qui répète b fois le test conditionnel sous l'hypothése nul
 conditionnal_test_under_null = function(a,n,n0.,n.0,b){
   quant = qhyper(1-a, n.0, n-n.0,n0., lower.tail = TRUE, log.p = FALSE)
   tab = rhyper(b,n.0,n-n.0, n0.)
@@ -6,15 +6,7 @@ conditionnal_test_under_null = function(a,n,n0.,n.0,b){
   mass_fuction = dhyper(x,n.0,n-n.0,n0.,log = FALSE)
   hist(tab,probability=TRUE,breaks = seq(min(tab)-0.5, max(tab)+0.5, by = 1))
   lines(x,mass_fuction,type ="h",col="red")
-  for (i in 1:b){
-    if (tab[i]>quant){
-    tab[i]=1
-    }
-  else{
-    tab[i]=0
-    }
-  }
-  risk_exp=sum(tab)/b
+  risk_exp <- mean(tab > quant)
   print(quant)
   print(tab)
   return(risk_exp)
@@ -26,19 +18,20 @@ conditionnal_test_under_null(0.05,100,20,50,1000)
 vect_a=c()
 x=seq(0, 1, by = 0.05)
 for(a in seq(0, 1, by = 0.05)){
-  vect_a = c(vect_a,conditionnal_test_under_null(a,100,20,50,1000))
+  vect_a = c(vect_a,conditionnal_test_under_null(a,100,20,50,10000))
 }
 plot(x,vect_a)
+abline(0,1,col='red')
 
 #Variation du risque de première espèce empirique en fonction de n0.     
 vect_n0.=c()
 x_2=seq(0, 100, by = 10)
 for(n0. in seq(0, 100, by = 10)){
-  vect_n0. = c(vect_n0.,conditionnal_test_under_null(0.1,100,n0.,50,1000))
+  vect_n0. = c(vect_n0.,conditionnal_test_under_null(0.1,100,n0.,50,10000))
 }
 plot(x_2,vect_n0.)
 
-
+#Fonction qui répète b fois le test inconditionnel 
 unconditionnal_test = function(a,n,p ,b){
   tab = rmultinom(b,n,p)
   vect_n00 = c()
@@ -54,7 +47,6 @@ unconditionnal_test = function(a,n,p ,b){
     vect_n00[i]=0
     }
   }
-'print(vect_n00)' 
 print(sum(vect_n00)/b)
 }
 
@@ -76,4 +68,49 @@ for(p00 in z){
 }
  plot(z,vect_p00)
 
+#Fonction qui calcule la distribution de la pvalue conditionellement et génére b pvalues indépendante 
+#et affiche la fonction de répartirion théorqiue, empirique et uniforme ainsi que les tirages de pvalues 
+conditionnal_pvalue_under_null = function(n,n0.,n.0,b){
+   vect_pval = c()
+   vect_Bpval = c()
+   vect_repF = c()
+   vect_repE = c()
+   support = max(0,n0.+n.0-n):min(n0.,n.0)
+   tab = rhyper(b,n.0,n-n.0, n0.)
+   for(k in support){
+     pk=0
+     for(i in k:min(n0.,n.0)){
+       pk = pk+choose(n.0,i)*choose(n-n.0,n0.-i)/choose(n, n0.)
+     }
+     vect_pval=c(vect_pval,pk)
+   }
+   for(i in tab){
+     vect_Bpval = c(vect_Bpval,vect_pval[match(i,support)])
+   }
+   for(i in rev(vect_pval)){
+     pv = 0
+     for(pk in vect_pval){
+       if(pk<=i){
+         j=which(vect_pval==pk)
+         pv = pv + choose(n.0,support[j])*choose(n-n.0,n0.-support[j])/choose(n,n0.)
+       }
+     }
+     vect_repF=c(vect_repF,pv)
+   }
+   vect_repE=ecdf(vect_Bpval)
+   vect_repF = unique(vect_repF)
+   par(mfrow=c(1,2))
+   plot(support,vect_pval,type='l')
+   lines(vect_Bpval,col='red',type='p')
+   lines(support,runif(length(support), min = 0, max = 1),type="p",col="blue")
+   f = stepfun(rev(vect_pval), c(0, vect_repF))
+   plot(c(0,1), c(0,1),type="n",xlab="t",ylab="F(t)")
+   uniform = punif(support, min = 0, max = 1, lower.tail = TRUE, log.p = FALSE)
+   lines(support,uniform,col="red")
+   lines(vect_repE,col="pink")
+   lines(f,col="green")
+ }
+conditionnal_pvalue_under_null(100,40,40,10000)
+?stepfun 
+?ecdf
 
